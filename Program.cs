586903436fe321f9494d2b2345eb1f7f7ba50d82
vika-sk <viktoriya-skryabina@mail.ru>
2023-03-14 00:23:23 +0300

    zadacha36

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿// Задача 34: Задайте массив заполненный случайными
-// положительными трёхзначными числами. Напишите программу, 
-//которая покажет количество чётных чисел в массиве.
-//[345, 897, 568, 234] -> 2  
+﻿//Задача 36: Задайте одномерный массив,
+// заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
 
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
 
 int size = ReadInt("Введите размерность массива: ");
+int min = ReadInt("Введите минимальное число массива: ");
+int max = ReadInt("Введите максимальное число массива: ");
 int [] numbers = new int[size];
 
 FillArrayRandomNumbers(numbers);
@@ -13,29 +17,19 @@ int result = 0;
 
 for (int i = 0; i < numbers.Length; i++)
 {
-    if (numbers[i] % 2 == 0)
+    if (i % 2 != 0)
     {
-        result++;
+        result += numbers[i];
     }
 }
-if (result % 10 == 1)
-{
-    Console.WriteLine($"В массиве {result} четное число");
-}
-if (result % 10 == 2 || result % 10 == 3 || result % 10 == 4)
-{
-    Console.WriteLine($"В массиве {result} четных числа");
-}
-else
-
-    Console.WriteLine($"В массиве {result} четных чисел");
+Console.WriteLine($"Сумма элементов нечетных позиций = {result}");
 
 // Методы
 void FillArrayRandomNumbers(int [] array) //Заполнение массива
 {
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(99, 999);
+        array[i] = new Random().Next(min, max);
     }
 }
 
@@ -47,7 +41,6 @@ void PrintArray(int[] array) //Вывод массива на экран
     }
     Console.WriteLine();
 }
-
 
 int ReadInt(string message)  //Функция ввода
 {
